ba2473565b0481356b8ab62487b226a585b86139
daurio@pactosolucoes.com.br <daurio@pactosolucoes.com.br>
2020-05-25 21:33:28 -0300

    Rotas

---
+++ src/components/error/Erro404.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Erro404 extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Deu pau (404)...</h1>
+            </div>
+        );
+    }
+}
+
+export default Erro404;
